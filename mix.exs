d6089cac7bc303b6de0d7b5a47af66c3657744d8
nikolauska <nikolauska1@gmail.com>
2017-07-21 17:59:13 +0300

    Update excoveralls

--- mix.exs
+++ mix.exs
@@ -64,8 +64,8 @@ defmodule TomlElixir.Mixfile do
     [
       {:dialyxir, "~> 0.5", only: :dev, runtime: false},
       {:credo, "~> 0.8.3", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.5.7", only: :test, runtime: false},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.7.1", only: :test, runtime: false},
       {:inch_ex, ">= 0.0.0", only: :docs}
     ]
   end
